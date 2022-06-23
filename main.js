2b768f79d259a0bf4cb323fe5d7904c592498565
mohamed-azzouz98 <azzouz.mohamed238@gmail.com>
2022-06-23 17:29:51 +0200

    Add button and booster

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let cookies = 0;
 let buttonClick = document.querySelector("#buttonClick");
+number = 1;
 
 
-buttonClick.addEventListener("click", function coockieClick(num = 1){
-    cookies += 1;
+buttonClick.addEventListener("click", function coockieClick(){
+    cookies += number;
     document.getElementById("cookies").innerHTML = cookies;
 });
 
@@ -14,22 +15,25 @@ let buyCursor = document.querySelector("#buyCursor");
 buyCursor.addEventListener("click", () =>{
     let cursorCost = Math.floor(10 * Math.pow(2,cursors));     //works out the cost of this cursor
     if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-        pointCursors = pointCursors + 1 ;
+        cursors += 1;                                   //increases number of cursors
+        pointCursors += 1 ;
     	cookies = cookies - cursorCost;                          //removes the cookies spent
         document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+        number += 1;
+        document.getElementById("coockiesPerClick").innerHTML = number + ' cookies par clique';
 
     };
     let nextCursorsCost = Math.floor(10 * Math.pow(2,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCursorsCost;  //updates the cursor cost for the user
+
+
 });
 
 let booster = 0;
 let pointBooster = 0;
 
 let buyBooster = document.querySelector("#buyBooster");
-
 
 buyBooster.addEventListener("click", () => {
     let boosterCost = Math.floor(50 * Math.pow(2,booster));
@@ -42,11 +46,14 @@ buyBooster.addEventListener("click", () => {
 	};
 	let nextBoosterCost = Math.floor(50 * Math.pow(2,booster))
 	document.getElementById('boosterCost').innerHTML = nextBoosterCost;
+
+    
 });
 
 window.setInterval(function(){
 	
-	document.getElementById('cookiesSecond').innerHTML = pointCursors + pointBooster + ' par seconde';
-	cookieClick(pointCursors + pointBooster);
-	
+    document.getElementById('cookiesSecond').innerHTML =  pointBooster + ' cookies par seconde';
+    cookies += pointBooster;
+    document.getElementById("cookies").innerHTML = cookies;
+    
 }, 1000);
